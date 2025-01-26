a8721f0f1871f6525a1273195cfa3c5b5774f117
Patrickaalves <patrickalvessc@gmail.com>
2025-01-26 14:29:58 -0300

    Adicionando novos endpoint

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,15 +1,64 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.dtos.CourseRecordDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
     final CourseRepository courseRepository;
 
-    public CourseServiceImpl(CourseRepository courseRepository) {
+    final ModuleRepository moduleRepository;
+
+    final LessonRepository lessonRepository;
+
+    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository) {
         this.courseRepository = courseRepository;
+        this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelList = moduleRepository.findallModulesIntoCourse(courseModel.getCourseId());
+        if (!moduleModelList.isEmpty()) {
+            for (ModuleModel module : moduleModelList) {
+               List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+               if (!lessonModelList.isEmpty()) {
+                    lessonRepository.deleteAll(lessonModelList);
+               }
+            }
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        courseRepository.delete(courseModel);
+    }
+
+    @Override
+    public CourseModel save(CourseRecordDto courseRecordDto){
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseRecordDto, courseModel);
+        courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        return courseRepository.save(courseModel);
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return courseRepository.existsByName(name);
     }
 }
