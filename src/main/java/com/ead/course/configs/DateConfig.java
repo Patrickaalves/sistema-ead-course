23a604249ba6ebb557070707f11036a9f62b9b5e
Patrickaalves <patrickalvessc@gmail.com>
2025-01-28 07:47:03 -0300

    Ajustando paginacao global

--- src/main/java/com/ead/course/configs/DateConfig.java
+++ src/main/java/com/ead/course/configs/DateConfig.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import java.time.format.DateTimeFormatter;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.*;
+
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class DateConfig {
 
     public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
