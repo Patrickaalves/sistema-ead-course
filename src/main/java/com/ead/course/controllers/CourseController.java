5f404a6b8ad0a7bcaa6dc378e61e8684a1607bc3
Patrickaalves <patrickalvessc@gmail.com>
2025-02-06 20:29:55 -0300

    Adicionando validacoes customizadas

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -4,11 +4,15 @@ import com.ead.course.dtos.CourseRecordDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.specifications.SpecificationTemplate;
+import com.ead.course.validations.CourseValidator;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -17,16 +21,23 @@ import java.util.UUID;
 @RequestMapping("/courses")
 public class CourseController {
 
+    Logger logger = LogManager.getLogger(CourseController.class);
+
     final CourseService courseService;
 
-    public CourseController(CourseService courseService) {
+    final CourseValidator courseValidator;
+
+    public CourseController(CourseService courseService, CourseValidator courseValidator) {
         this.courseService = courseService;
+        this.courseValidator = courseValidator;
     }
 
     @PostMapping
-    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseRecordDto courseRecordDto) {
-        if (courseService.existsByName(courseRecordDto.name())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Course name is already taken!");
+    public ResponseEntity<Object> saveCourse(@RequestBody CourseRecordDto courseRecordDto, Errors errors) {
+        logger.debug("POST saveCourse courseRecordDto received : {}", courseRecordDto);
+        courseValidator.validate(courseRecordDto, errors);
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseRecordDto));
     }
